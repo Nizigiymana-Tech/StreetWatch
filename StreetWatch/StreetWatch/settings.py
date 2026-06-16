97e6a932f70c0d17e61038033d3e2b8c0cacac8a
Nizigiymana-Tech <junkacc830@gmail.com>
2026-06-16 12:53:58 -0400

    web dev fair project

--- StreetWatch/StreetWatch/settings.py
+++ StreetWatch/StreetWatch/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e#%xicg^=+5&y0vff0w4&$qhwpjm9=chb4a%*2p@tr*57*6!u_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
